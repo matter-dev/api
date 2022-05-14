9f344437246b74fe29ac426923c5c8bd6c4b22ef
Prateek Soni <prateek_soni@outlook.com>
2022-05-15 02:15:23 +0530

    add origin to cors
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://matter-wheat.vercel.app",
+  })
+);
 
 app.use(express.json());
 
